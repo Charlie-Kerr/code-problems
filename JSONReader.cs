1d1736efdad1ebe87f1e496f65a5637dd7a04d04
“Charlie-Kerr” <charlie@mcalpine.tv>
2025-02-15 12:54:14 +0000

    Added JSONReader class to new file.

---
+++ JSONReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeProblems
+{
+    public class JSONReader
+    {
+        public static List<string> Read(string path)
+        {
+            return new List<string>();
+        }
+    }
+}
